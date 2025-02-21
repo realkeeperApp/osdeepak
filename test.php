4b5cdc33bc5e88abd66cbbaebba42c7ce6409327
Deepanshu Bisht <95918264+deepansh0403@users.noreply.github.com>
2025-02-21 17:27:11 +0530

    new database connecton

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
+<?php
+$conn = mysqli_connect("localhost","grac_deepak","deepak@2","grac_deepak");
+if($conn){
+    echo "Database connection established";
+}else{
+    echo "Failed";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +14,7 @@
     <title>Document</title>
 </head>
 <body>
+    <h2></h2>
     <h1>This is Static Data</h1>
     <h1 class="Value"></h1>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
